d3cff8e2cb5bbd0531079612565ef8d0adb08e1f
Hugo Oliveira <hugooliveira.dev@proton.me>
2025-02-23 18:11:04 -0300

    feat: add notification entity

---
+++ src/domain/notification/enterprise/entities/notification.ts
@@ -0,0 +1,48 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueIdentifier } from '@/core/entities/value-objects/unique-identifier'
+import { Optional } from '@/core/types/optional'
+
+interface NotificationProps {
+  recipientId: UniqueIdentifier
+  title: string
+  content: string
+  createdAt: Date
+  readAt?: Date
+}
+
+export class Notification extends Entity<NotificationProps> {
+  get recipientId() {
+    return this._props.recipientId
+  }
+
+  get title() {
+    return this._props.title
+  }
+
+  get content() {
+    return this._props.content
+  }
+
+  get createdAt() {
+    return this._props.createdAt
+  }
+
+  get readAt() {
+    return this._props.readAt
+  }
+
+  static create(
+    props: Optional<NotificationProps, 'createdAt'>,
+    id?: UniqueIdentifier,
+  ) {
+    const notification = new Notification(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+
+    return notification
+  }
+}
